92114e8f8ac5e88f4824fdc58c6b8af4cdb39130
Consoom <dumpdiedump@gmail.com>
2022-04-26 21:25:14 +0200

    Readded color emoji support: install libxft-bgra

--- config.h
+++ config.h
@@ -52,7 +52,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,         NULL,     0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,         NULL,     1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,         NULL,     0,         0,          1,           0,        -1 },
-        { NULL,      "spterm",	   NULL,     SPTAG(0),  1,	    0,           0,        -1 },
+        { NULL,      "spterm",	   NULL,     SPTAG(0),  1,	    1,           0,        -1 },
         //{ NULL,    "spfm",	   NULL,     SPTAG(1),  1,	    0,           0,	   -1 },
         //{ NULL,    "keepassxc",  NULL,     SPTAG(2),  0,          0,           0,	   -1 },
 
